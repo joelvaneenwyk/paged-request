939c91f1eab18b37a998d419c0d232ccf2e4b9f4
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2025-02-15 13:33:37 -0800

    feat: enhance paginated request handling with detailed JSDoc comments and improved parameter management
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- index.js
+++ index.js
@@ -1,26 +1,64 @@
-'use strict';
+/**
+ * Wrapper around axios to make it easier to follow paginated links.
+ *
+ * @module paged-request
+ */
 
 const axios = require('axios');
 
+/**
+ * @typedef {(url: string, response: Axios.AxiosXHR<any>, accumulator: Accumulator) => Promise<string|null>} NextUrlFunction
+ * @typedef {Axios.AxiosXHRConfig<any> | NextUrlFunction} ConfigOrNext
+ *
+ * @typedef {Object} Accumulator
+ * @property {string} orig - Original URL
+ * @property {any} options - Request options
+ * @property {any[]} pages - Accumulated pages
+ * @property {string[]} urls - Accumulated URLs
+ */
+
+/**
+ * Makes paginated requests to the specified URL.
+ *
+ * @param {any | null} url - The initial URL to request
+ * @param {ConfigOrNext} options - Axios request configuration
+ * @param {ConfigOrNext | undefined} next - Function to get the next URL
+ * @returns {Promise<Accumulator>} Accumulated response data
+ */
 module.exports = async function(url, options, next) {
   if (typeof url !== 'string') {
     return Promise.reject(new TypeError('expected "url" to be a string'));
   }
 
+  /** @type {Axios.AxiosXHRConfig<any> | null} */
+  let in_options = null;
+  /** @type {NextUrlFunction | null} */
+  let in_next = null;
+
   if (typeof options === 'function') {
-    next = options;
-    options = null;
+    in_next = options;
+  } else if (typeof options === 'object' && options !== null) {
+    in_options = options;
+    if (typeof next === 'function') {
+      in_next = next;
+    }
   }
 
-  const opts = Object.assign({}, options);
+  const opts = Object.assign({}, in_options);
+
+  /** @type {Accumulator} */
   const acc = { orig: url, options, pages: [], urls: [] };
   let res;
 
-  while (url && typeof url === 'string' && !acc.urls.includes(url)) {
-    acc.urls.push(url);
-    res = await axios.get(url, opts);
-    url = await next(url, res, acc);
+  /** @type {string | null} */
+  let next_url = url;
+  while (next_url && typeof next_url === 'string' && !acc.urls.includes(next_url)) {
+    acc.urls.push(next_url);
+    res = await axios.get(next_url, opts);
     acc.pages.push(res);
+    if (in_next) {
+      next_url = await in_next(next_url, res, acc);
+    }
   }
 
   return acc;
